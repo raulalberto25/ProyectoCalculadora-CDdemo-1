0e2d0ea4d79b19386d84ccf1f878fdf41d4ec941
JJ Ruescas <juanjose.ruescas@gmail.com>
2017-09-02 14:31:18 -0400

    Initial Project commit

---
+++ ProyectoCalculadora/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoCalculadora.Models;
+
+namespace ProyectoCalculadora.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculadoraModel model)
+        {
+            if (model.operacion == "+")
+            {
+                int resultado = model.numero1 + model.numero2;
+                ViewBag.resultado = resultado;
+            }
+            else
+            {
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ProyectoCalculadora/Models/CalculadoraModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Models
+{
+    public class CalculadoraModel
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+        public string operacion { get; set; }
+    }
+}
